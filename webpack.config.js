e69b8d6fe3c40f30e5ca8af89ca4b0de39a49129
Cloudswillopenforme <Christine.Tikhonowa@gmail.com>
2019-08-09 12:18:55 +0300

    gh-pages module for webpack added

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
-module.exports = [{
+import GhPagesWebpackPlugin from 'gh-pages-webpack-plugin';
+
+export default [{
     entry: './styles/app.scss',
     output: {
       // This is necessary for webpack to compile
       // But we never use style-bundle.js
       filename: 'style-bundle.js',
     },
+    plugins: [
+        new GhPagesWebpackPlugin({
+            path: './public',
+        })
+    ],
     module: {
       rules: [
         {
