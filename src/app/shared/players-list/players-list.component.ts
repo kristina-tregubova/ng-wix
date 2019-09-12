3064336d2df83b35495b6749a1ba0eb7d030b65e
Cloudswillopenforme <Christine.Tikhonowa@gmail.com>
2019-09-12 19:15:30 +0300

    sorting function is implemented

--- src/app/shared/players-list/players-list.component.ts
+++ src/app/shared/players-list/players-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IPlayer } from 'src/app/core/models/IPlayer';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-players-list',
@@ -11,10 +13,31 @@ export class PlayersListComponent implements OnInit {
   @Input() items$: Observable<any>;
   @Input() isLoading$: Observable<boolean>;
 
+  order: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
-    // this.items$.subscribe(val=>console.log(val))
+    this.items$ = this.handleSorting('name', 'letters');
+  }
+
+  handleSorting(field: string, sortType: string) {
+
+    this.order = !this.order;
+
+    this.items$ = this.items$.pipe(
+      map((itemArr: IPlayer[]) => {
+        return itemArr = itemArr.sort((a: IPlayer, b: IPlayer) => {
+          if (sortType === 'letters') {
+            return (this.order) ? a[field].localeCompare(b[field]) : b[field].localeCompare(a[field]);
+          } else {
+            return (this.order) ? a[field] - b[field] : b[field] - a[field];
+          }
+        });
+      }),
+    );
+
+    return this.items$;
   }
 
 }
