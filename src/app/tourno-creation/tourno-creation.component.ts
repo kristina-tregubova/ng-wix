8fbaac13941e331362f80d9507ad1025accc467c
Cloudswillopenforme <Christine.Tikhonowa@gmail.com>
2019-09-18 23:19:17 +0300

    validation for steps added

--- src/app/tourno-creation/tourno-creation.component.ts
+++ src/app/tourno-creation/tourno-creation.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormBuilder, AbstractControl } from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-tourno-creation',
   templateUrl: './tourno-creation.component.html',
-  styleUrls: ['./tourno-creation.component.scss']
+  styleUrls: ['./tourno-creation.component.scss'],
+  providers: [{
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: {showError: true}
+  }]
 })
 export class TournoCreationComponent implements OnInit {
 
-  isLinear = false;
+  isLinear = true;
   formGroup: FormGroup;
+  /** Returns a FormArray with the name 'formArray'. */
+  get formArray(): AbstractControl | null { return this.formGroup.get('formArray'); }
 
   constructor(
     private _formBuilder: FormBuilder,
@@ -19,16 +25,16 @@ export class TournoCreationComponent implements OnInit {
     this.formGroup = this._formBuilder.group({
       formArray: this._formBuilder.array([
         this._formBuilder.group({
-          name: [''],
+          name: ['', Validators.required],
           tournamentType: [''],
-          country: [''],
-          game: [''],
+          country: ['', Validators.required],
+          game: ['', Validators.required],
           playerType: [''],
           description: [''],
         }),
         this._formBuilder.group({
-          startDate: [''],
-          endDate: [''],
+          startDate: ['', Validators.required],
+          endDate: ['', Validators.required],
           entryFee: [''],
           prize: [''],
         }),
